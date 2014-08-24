96608542c9cbb5e49bbbf3367429b2beb6e2f01d
Russell Andrew Edson <russell.andrew.edson@gmail.com>
2014-08-24 16:50:37 +0930

    Update ConsoleDriver.scala

--- src/main/scala/ConsoleDriver.scala
+++ src/main/scala/ConsoleDriver.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014 Russell Edson
+ * Copyright (c) 2013-2014 Russell Andrew Edson
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
