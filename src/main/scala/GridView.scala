4112c71f3b787ef3e78bd996ec8e3c55845f2e66
RussellAndrewEdson <russell.andrew.edson@gmail.com>
2014-09-14 16:28:41 +0930

    Refactored GridView methods + documentation

--- src/main/scala/GridView.scala
+++ src/main/scala/GridView.scala
@@ -21,45 +21,74 @@
 * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-// Code for the GridView.
-
 import swing._
 import Swing._
 
 import java.awt.Color
 
-class GridView(rowCount: Int, columnCount: Int) 
+/** A grid in the UI that contains the displayable cells for the link-flow
+  * and allocations. The size of this grid corresponds directly to the
+  * size of the Transportation problem to be solved (supplies x demands).
+  *
+  * @constructor Create a new grid of cells in the UI with the given number 
+  *              of rows and columns.
+  * @param rowCount The number of rows for the grid.
+  * @param columnCount The number of columns for the grid.
+  *
+  * @author Russell Andrew Edson, <russell.andrew.edson@gmail.com>
+  * @version 0.1
+  */
+class GridView(val rowCount: Int, val columnCount: Int) 
     extends GridPanel(rowCount, columnCount) {
-  
+
+  /** The indices for the displayed grid. */
+  val indices = for {
+      i <- 0 until rowCount
+      j <- 0 until columnCount
+    } yield (i,j)
+
+  /* We surround the grid with a line border to distinguish it from the
+   * rest of the tableau elements.
+   */
   border = LineBorder(Color.BLACK)
 
-  private val linkFlow = Array.ofDim[GridCell](rowCount, columnCount)
-  for (i <- 0 until linkFlow.length; j <- 0 until linkFlow(i).length) {
-    linkFlow(i)(j) = new GridCell()
-  }
+  /* The cells are instantiated and placed in the grid one by one. */
+  private val cells = Array.ofDim[GridCell](rowCount, columnCount)
+  indices.foreach { p => cells(p._1)(p._2) = new GridCell() }
+  indices.foreach { p => contents += cells(p._1)(p._2) }
 
-  for (i <- 0 until linkFlow.length; j <- 0 until linkFlow(i).length) {
-    contents += linkFlow(i)(j)
-  }
-
+  /** Returns a 2-dimensional array containing the user-supplied values 
+    * for the link-flow costs.
+    *
+    * (This method is provided for convenience; the output can be
+    * immediately used to instantiate a TransportationTableau object.)
+    *
+    * @return A 2D array of integer values for the link-flow costs.
+    */
   def getLinkFlowCosts(): Array[Array[Int]] = {
     // Error checking?
-    val linkFlowCosts = Array.ofDim[Int](linkFlow.length, linkFlow(0).length)
-    for (i <- 0 until linkFlowCosts.length) {
-      for (j <- 0 until linkFlowCosts(i).length) {
-        linkFlowCosts(i)(j) = linkFlow(i)(j).getCost()
-      } 
+    val linkFlowCosts = Array.ofDim[Int](rowCount, columnCount)
+    indices.foreach { 
+      p => linkFlowCosts(p._1)(p._2) = cells(p._1)(p._2).getCost()
     }
 
     linkFlowCosts
   }
 
+  /** Sets the displayed allocations for the grid to the values in the
+    * given 2-dimensional array.
+    *
+    * (This method is provided for convenience; the input can simply be
+    * the output of the .allocations() method of the current 
+    * TransportationTableau object.)
+    *
+    * @param allocations A 2-dimensional integer array of allocation values.
+    */
   def setAllocations(allocations: Array[Array[Int]]) {
     // Error checking?
-    for (i <- 0 until linkFlow.length; j <- 0 until linkFlow(i).length) {
-      linkFlow(i)(j).setAllocation(allocations(i)(j))
+    indices.foreach {
+      p => cells(p._1)(p._2).setAllocation(allocations(p._1)(p._2))
     }
-
-    // Might need to redraw the view here.
   }
+
 }
